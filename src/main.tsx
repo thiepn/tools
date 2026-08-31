5d61f8e047496c8c42c2eb56758ce22dc518b25a
thiepn <junsosmart@gmail.com>
2026-08-31 19:28:30 +0200

    Wire P4 file conversion routes at startup

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx';
 import {registerPdfPublicTools} from './registry/pdf-extension';
 import {registerDeviceDiagnosticTools} from './registry/device-extension';
 import {registerCalculatorTools} from './registry/calculator-extension';
+import {registerFileConversionTools} from './registry/file-conversion-extension';
 import './index.css';
 
 registerPdfPublicTools();
 registerDeviceDiagnosticTools();
 registerCalculatorTools();
+registerFileConversionTools();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
