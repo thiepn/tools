ed03cdf9969cce2e2afbb430314b8f7f26280347
thiepn <junsosmart@gmail.com>
2026-09-02 12:08:22 +0200

    Route image conversion through expanded workspace

--- src/registry/image-micro-extension.ts
+++ src/registry/image-micro-extension.ts
@@ -1,5 +1,5 @@
 import{lazy}from'react';
 import{PUBLIC_IMAGE_TASKS}from'../image/publicImageTasks';
 import{registerFamily}from'./register-family';
-const ImageMicroTools=lazy(()=>import('../tools/image-micro-tools/ImageMicroTools')),MainstreamConverters=lazy(()=>import('../tools/converters/MainstreamConverters'));
-export function registerImageMicroTools():void{registerFamily(PUBLIC_IMAGE_TASKS,'image','Image',ImageMicroTools,t=>({keywords:[...t.keywords,'image','photo'],component:t.id==='image-converter'?MainstreamConverters:ImageMicroTools}))}
+const ImageMicroTools=lazy(()=>import('../tools/image-micro-tools/ImageMicroTools')),ExpandedImageAudioConverter=lazy(()=>import('../tools/converters/ExpandedImageAudioConverter'));
+export function registerImageMicroTools():void{registerFamily(PUBLIC_IMAGE_TASKS,'image','Image',ImageMicroTools,t=>({keywords:[...t.keywords,'image','photo'],component:t.id==='image-converter'?ExpandedImageAudioConverter:ImageMicroTools}))}
