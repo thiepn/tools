fa97e6f0658ab623d07dc99297a499085c226e97
thiepn <junsosmart@gmail.com>
2026-09-05 18:59:31 +0200

    Apply R17 strict A-tier upgrades after prior quality layers

--- src/registry/register-all.ts
+++ src/registry/register-all.ts
@@ -21,5 +21,6 @@ import{applySTierBUplift}from'./s-tier-b-extension';
 import{applySTierAUplift}from'./s-tier-a-extension';
 import{applyStrictSTierUpgrades}from'./strict-s-tier-extension';
 import{applyStrictBToSUpgrades}from'./strict-b-to-s-extension';
+import{applyStrictAToSUpgrades}from'./strict-a-to-s-extension';
 const REGISTRARS=[registerPdfPublicTools,registerDeviceDiagnosticTools,registerCalculatorTools,registerFileConversionTools,registerImageMicroTools,registerMediaMicroTools,registerTextStudyTools,registerPrivacyDevTools,registerEverydayTools,registerP11Tools,registerP12Tools,registerP13Tools,registerP14Tools,registerP15Tools,registerP16Tools,registerP17Tools,registerP18Tools,registerP19Tools,registerP20Tools];
-export function registerAllPublicTools(){for(const register of REGISTRARS)register();applySTierBUplift();applySTierAUplift();applyStrictSTierUpgrades();applyStrictBToSUpgrades()}
+export function registerAllPublicTools(){for(const register of REGISTRARS)register();applySTierBUplift();applySTierAUplift();applyStrictSTierUpgrades();applyStrictBToSUpgrades();applyStrictAToSUpgrades()}
